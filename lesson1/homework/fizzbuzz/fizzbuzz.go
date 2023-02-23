acf79f3c9f3e61d06f2ea08ba5880b1cf4d68809
dimosha19 <shilnikov_dmitriy@mail.ru>
2023-02-23 18:18:09 +0500

    Шильников Дмитрий HW1

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,16 @@
 package fizzbuzz
 
+import "strconv"
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	if i%3 == 0 {
+		if i%5 == 0 {
+			return "FizzBuzz"
+		} else {
+			return "Fizz"
+		}
+	} else if i%5 == 0 {
+		return "Buzz"
+	}
+	return strconv.FormatInt(int64(i), 10)
 }
