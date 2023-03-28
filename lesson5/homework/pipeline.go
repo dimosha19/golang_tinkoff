c73e844ad7deaad330c19c3c18a90c15f3263e6c
dimosha19 <shilnikov_dmitriy@mail.ru>
2023-03-28 17:28:40 +0500

    Каналы без размеров

--- lesson5/homework/pipeline.go
+++ lesson5/homework/pipeline.go
@@ -16,12 +16,11 @@ type Stage func(in In) (out Out)
 type Pack struct {
 	seqNum int
 	stage  int
-	ctx    context.Context
 	value  any
 }
 
-func NewPack(ctx context.Context, val any, num int) *Pack {
-	return &Pack{num, 0, ctx, val}
+func NewPack(val any, num int) *Pack {
+	return &Pack{num, 0, val}
 }
 
 func (p *Pack) Push(stages ...Stage) *Pack {
@@ -45,27 +44,29 @@ func (p *Pack) Run(stages ...Stage) *Pack {
 	return p
 }
 
-func Collector(c <-chan *Pack) chan any {
-	res := make(chan any, 100)
-	defer close(res)
-	var b []*Pack
-	for i := range c {
-		b = append(b, i)
-	}
-	sort.Slice(b, func(i, j int) bool {
-		return b[i].seqNum <= b[j].seqNum
-	})
-	for i := range b {
-		res <- b[i].value
-	}
+func Collector(b []*Pack) <-chan any {
+	res := make(chan any)
+	go func() {
+		defer close(res)
+		sort.Slice(b, func(i, j int) bool {
+			return b[i].seqNum <= b[j].seqNum
+		})
+		for i := range b {
+			res <- b[i].value
+		}
+	}()
 	return res
 }
 
 func ExecutePipeline(ctx context.Context, in In, stages ...Stage) Out {
 	stp := make(chan any)
 	wg := sync.WaitGroup{}
+	type ans struct {
+		MU   sync.Mutex
+		data []*Pack
+	}
+	var c ans
 	ticket := 0
-	c := make(chan *Pack, 100)
 	for i := range in {
 		select {
 		case <-ctx.Done():
@@ -75,13 +76,15 @@ func ExecutePipeline(ctx context.Context, in In, stages ...Stage) Out {
 			wg.Add(1)
 			go func(i any, num int) {
 				defer wg.Done()
-				n := NewPack(ctx, i, num)
-				c <- n.Run(stages...)
+				n := NewPack(i, num)
+				n = n.Run(stages...)
+				c.MU.Lock()
+				c.data = append(c.data, n)
+				c.MU.Unlock()
 			}(i, ticket)
 			ticket++
 		}
 	}
 	wg.Wait()
-	close(c)
-	return Collector(c)
+	return Collector(c.data)
 }
