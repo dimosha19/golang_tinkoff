440f6d0f5670a7d0a5ad3feeee208b0a599b783d
dimosha19 <shilnikov_dmitriy@mail.ru>
2023-04-01 14:48:03 +0500

    Шильников Дмитрий HW5 - исправление

--- lesson5/homework/pipeline.go
+++ lesson5/homework/pipeline.go
@@ -11,7 +11,27 @@ type (
 
 type Stage func(in In) (out Out)
 
+func doStage(ctx context.Context, in <-chan any) <-chan any {
+	out := make(chan any)
+	go func() {
+		defer close(out)
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			for n := range in {
+				out <- n
+			}
+		}
+	}()
+	return out
+}
+
 func ExecutePipeline(ctx context.Context, in In, stages ...Stage) Out {
-	// TODO
-	return nil
+	empty := make(chan any)
+	defer close(empty)
+	for _, stage := range stages {
+		in = stage(doStage(ctx, in))
+	}
+	return in
 }
