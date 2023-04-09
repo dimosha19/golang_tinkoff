0b44bc3cceadd1da794e5fd32abd26296ba385ea
dimosha19 <shilnikov_dmitriy@mail.ru>
2023-04-09 16:55:37 +0500

    ~

--- lesson6/homework/internal/ads/ads.go
+++ lesson6/homework/internal/ads/ads.go
@@ -2,8 +2,8 @@ package ads
 
 type Ad struct {
 	ID        int64
-	Title     string
-	Text      string
+	Title     string `validate:"min:1 max:100"`
+	Text      string `validate:"min:1 max:500"`
 	AuthorID  int64
 	Published bool
 }
